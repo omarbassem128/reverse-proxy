27f9832d54dfe75227c9899317a604d3b6625b4a
Omar Bassem <omarbassem889@gmail.com>
2026-04-02 10:53:54 +0200

    refactor: rename fixed window file & methods

---
+++ backend/middleware/fixed-window-rate-limiter.go
@@ -0,0 +1,58 @@
+package middleware
+
+import (
+	"net"
+	"net/http"
+	"sync"
+	"time"
+)
+
+// this can be made as configurable parameters
+type FixedWindowRl struct {
+	clients map[string]int
+	mu      sync.Mutex
+}
+
+func (rl *FixedWindowRl) FixedWindowRateLimiter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		host, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			http.Error(w, "Internal service error", http.StatusInternalServerError)
+			return
+		}
+
+		rl.mu.Lock()
+		requests := rl.clients[host]
+		rl.clients[host]++
+		// mutex is being unlocked here (and not using defer) because next.ServeHTTP() is a blocking call.
+		// This means that if the call takes a while, it will block other users from having their IPs checked via the middleware, which defeats the purpose.
+		rl.mu.Unlock()
+
+		if requests < 5 {
+			next.ServeHTTP(w, r)
+		} else {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+		}
+
+	})
+}
+
+func NewFixedWindowRateLimiter() *FixedWindowRl {
+	//mutex is automatically ready to use.
+	rl := &FixedWindowRl{
+		clients: make(map[string]int),
+	}
+
+	go func() {
+		ticker := time.NewTicker(1 * time.Minute)
+
+		for range ticker.C {
+			rl.mu.Lock()
+			clear(rl.clients)
+			rl.mu.Unlock()
+		}
+
+	}()
+
+	return rl
+}
