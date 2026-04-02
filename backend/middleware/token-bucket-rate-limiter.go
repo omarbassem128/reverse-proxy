df23d42f3d9e584d65b2688b9d9457bfe529a787
Omar Bassem <omarbassem889@gmail.com>
2026-04-02 11:18:12 +0200

    fix: add race condition handling to token bucket

--- backend/middleware/token-bucket-rate-limiter.go
+++ backend/middleware/token-bucket-rate-limiter.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net"
 	"net/http"
 	"sync"
@@ -20,32 +19,37 @@ type TokenBuckerRl struct {
 
 func (tbrl *TokenBuckerRl) TokenBucketRateLimiter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tbrl.mu.Lock()
+
 		host, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			http.Error(w, "internal service error", http.StatusInternalServerError)
+			tbrl.mu.Unlock()
 			return
 		}
 
 		currentClient := tbrl.clients[host]
 		if currentClient == nil {
 			tbrl.clients[host] = &ClientState{tokens: 4, lastVisited: time.Now()}
+			tbrl.mu.Unlock()
 			next.ServeHTTP(w, r)
 			return
-		}
-
-		lastVisited := currentClient.lastVisited
-		difference := time.Since(lastVisited)
-		currentClient.lastVisited = time.Now()
-		differenceInt := int(difference.Seconds())
-		addition := differenceInt + currentClient.tokens
-		newTokens := min(addition, 5)
-		currentClient.tokens = newTokens
-
-		if currentClient.tokens > 0 {
-			next.ServeHTTP(w, r)
-			currentClient.tokens--
-			fmt.Printf("remaining tokens: %d\n", currentClient.tokens)
 		} else {
+			lastVisited := currentClient.lastVisited
+			difference := time.Since(lastVisited)
+			currentClient.lastVisited = time.Now()
+			differenceInt := int(difference.Seconds())
+			addition := differenceInt + currentClient.tokens
+			newTokens := min(addition, 5)
+			currentClient.tokens = newTokens
+		}
+		
+		if currentClient.tokens > 0 {
+			currentClient.tokens--
+			tbrl.mu.Unlock()
+			next.ServeHTTP(w, r)
+		} else {
+			tbrl.mu.Unlock()
 			http.Error(w, "Too many requests", http.StatusTooManyRequests)
 		}
 
